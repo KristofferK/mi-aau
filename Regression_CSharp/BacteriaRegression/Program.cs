1021089af437823ccd1b2ff98d99c3b609491ff8
Kristoffer <kknuds19@student.aau.dk>
2019-09-20 22:29:07 +0200

    Add c# project with Accord

---
+++ Regression_CSharp/BacteriaRegression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BacteriaRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
