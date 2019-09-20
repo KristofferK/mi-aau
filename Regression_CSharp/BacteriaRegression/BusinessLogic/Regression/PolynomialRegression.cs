e788dea56e21c33bbd0381cdd801f664161333bb
Kristoffer <kknuds19@student.aau.dk>
2019-09-21 00:28:37 +0200

    Add poly regression

---
+++ Regression_CSharp/BacteriaRegression/BusinessLogic/Regression/PolynomialRegression.cs
@@ -0,0 +1,34 @@
+﻿using Accord.Statistics.Models.Regression.Linear;
+using BacteriaRegression.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BacteriaRegression.BusinessLogic.Regression
+{
+    class PolynomialRegression : IRegressionModel
+    {
+        public int Degree { get; set; }
+
+        public PolynomialRegression(int degree)
+        {
+            Degree = degree;
+        }
+
+        public RegressionResult PerformRegression(double[] trainX, double[] trainY, double[] testX, double[] testY)
+        {
+            var ls = new PolynomialLeastSquares()
+            {
+                Degree = Degree
+            };
+            Accord.Statistics.Models.Regression.Linear.PolynomialRegression regression = ls.Learn(trainX, trainY);
+
+            return new RegressionResult
+            {
+                FormulaUsed = regression.ToString("N1"),
+                PredictedMeasurements = testX.Select(regression.Transform).ToArray()
+            };
+        }
+    }
+}
--- Regression_CSharp/BacteriaRegression/Program.cs
+++ Regression_CSharp/BacteriaRegression/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BacteriaRegression.BusinessLogic;
 using BacteriaRegression.BusinessLogic.Regression;
+using BacteriaRegression.Models;
 using System;
 using System.Linq;
 
@@ -15,12 +16,26 @@ namespace BacteriaRegression
                 Console.WriteLine(bacterium);
             }
 
-            Regression regression = new Regression(new LinearRegression());
-            var regressionResult = regression.PerformRegression(bacteria.ElementAt(0));
-            Console.WriteLine("Used " + regressionResult.FormulaUsed);
-            for (var i = 0; i < regressionResult.TestTimestamp.Length; i++)
+            Regression reg;
+
+            reg = new Regression(new LinearRegression());
+            PrintResult("Linear", reg.PerformRegression(bacteria.ElementAt(1)));
+
+            for (var i = 2; i < 15; i++)
             {
-                Console.WriteLine($"Timestamp {regressionResult.TestTimestamp[i]} is {regressionResult.TestMeasurements[i]} and predicted {regressionResult.PredictedMeasurements[i]}");
+                reg = new Regression(new PolynomialRegression(i));
+                PrintResult($"Poly{i}", reg.PerformRegression(bacteria.ElementAt(1)));
+                Console.ReadLine();
+            }
+        }
+
+        static void PrintResult(string title, RegressionResult result)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(title + ". Used " + result.FormulaUsed);
+            for (var i = 0; i < result.TestTimestamp.Length; i++)
+            {
+                Console.WriteLine($"Timestamp {result.TestTimestamp[i]} is {result.TestMeasurements[i]} and predicted {result.PredictedMeasurements[i]}");
             }
         }
     }
