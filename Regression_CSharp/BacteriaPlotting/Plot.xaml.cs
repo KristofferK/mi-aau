0e1660acfba36c831d6e9e9c15100d5909809979
Kristoffer <kknuds19@student.aau.dk>
2019-10-09 14:11:22 +0200

    Add name to series

--- Regression_CSharp/BacteriaPlotting/Plot.xaml.cs
+++ Regression_CSharp/BacteriaPlotting/Plot.xaml.cs
@@ -17,19 +17,21 @@ namespace BacteriaPlotting
 
             var bacteria = BacteriaImporter.Import(15);
 
-            
+            Series1.Title = $"{bacteria[3].ASV} vs {bacteria[12].ASV}";
+            Series2.Title = $"{bacteria[0].ASV} vs {bacteria[1].ASV}";
 
-            var r = new Random();
-            ValuesA = new ChartValues<ObservablePoint>();
+            // BacteriaChart.Series.
 
             for (int i = 0; i < bacteria[3].Measurements.Count; i++)
             {
                 ValuesA.Add(new ObservablePoint(bacteria[3].Measurements[i], bacteria[12].Measurements[i]));
+                ValuesB.Add(new ObservablePoint(bacteria[0].Measurements[i], bacteria[1].Measurements[i]));
             }
 
             DataContext = this;
         }
 
-        public ChartValues<ObservablePoint> ValuesA { get; set; }
+        public ChartValues<ObservablePoint> ValuesA { get; set; } = new ChartValues<ObservablePoint>();
+        public ChartValues<ObservablePoint> ValuesB { get; set; } = new ChartValues<ObservablePoint>();
     }
 }
